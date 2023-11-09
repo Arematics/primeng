9f9e333258b7963a24b964704e6434cb1cf1ac4a
Enrico Messall <enrico.messall@arematics.com>
2023-11-09 16:24:36 +0100

    New angular version change one

--- server.ts
+++ server.ts
@@ -1,57 +1,19 @@
 import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import express from 'express';
-import { existsSync } from 'fs';
-import { join } from 'path';
-
-import { AppServerModule } from './src/main.server';
-
-// ssr DOM
-const domino = require('domino');
-const fs = require('fs');
-const path = require('path');
-// index from browser build!
-const template = fs.readFileSync(path.join('.', 'dist/primeng/browser', 'index.html')).toString();
-// for mock global window by domino
-const win = domino.createWindow(template);
-// mock
-global['window'] = win;
-// not implemented property and functions
-Object.defineProperty(win.document.body.style, 'transform', {
-    value: () => {
-        return {
-            enumerable: true,
-            configurable: true
-        };
-    }
-});
-// mock document
-global['document'] = win.document;
-// mock navigator
-global['navigator'] = win.navigator;
-// others mock
-global['CSS'] = null;
-// global['XMLHttpRequest'] = require('xmlhttprequest').XMLHttpRequest;
-global['Prism'] = null;
-// global google tag manager
-global['gtag'] = () => {};
+import { CommonEngine } from '@angular/ssr';
+import * as express from 'express';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
     const server = express();
     const distFolder = join(process.cwd(), 'dist/primeng/browser');
-    const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+    const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? join(distFolder, 'index.original.html') : join(distFolder, 'index.html');
 
-    // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
-    server.engine(
-        'html',
-        ngExpressEngine({
-            bootstrap: AppServerModule,
-            inlineCriticalCss: false
-        })
-    );
+    const commonEngine = new CommonEngine();
 
     server.set('view engine', 'html');
     server.set('views', distFolder);
@@ -66,9 +28,20 @@ export function app(): express.Express {
         })
     );
 
-    // All regular routes use the Universal engine
-    server.get('*', (req, res) => {
-        res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    // All regular routes use the Angular engine
+    server.get('*', (req, res, next) => {
+        const { protocol, originalUrl, baseUrl, headers } = req;
+
+        commonEngine
+            .render({
+                bootstrap,
+                documentFilePath: indexHtml,
+                url: `${protocol}://${headers.host}${originalUrl}`,
+                publicPath: distFolder,
+                providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
+            })
+            .then((html) => res.send(html))
+            .catch((err) => next(err));
     });
 
     return server;
@@ -94,4 +67,4 @@ if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
     run();
 }
 
-export * from './src/main.server';
+export default bootstrap;
